b272a048dc158e055e69664a849cbaee8293fa6a
KirdesMF <cedgourville@gmail.com>
2023-03-21 17:37:15 +0100

    wip: ejs template

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
 app.get("/invoices", (req: Request, res: Response) => {
-  res.send("Invoices pdf");
+  res.render("header");
 });
 
 app.listen(port, () => {
